b5e7afc1565cd1d70ed99a62dc59474d393bdbb1
Jamil-4khtar <jamil4khtar@gmail.com>
2025-07-22 00:17:11 +0530

    update: shadow to buttons

--- client-vwe/src/components/TemplateCard.jsx
+++ client-vwe/src/components/TemplateCard.jsx
@@ -4,33 +4,31 @@ import DOMPurify from "dompurify";
 function TemplateCard({ template, onEdit, openModal }) {
   return (
     <div>
-      <div className="relative">
-      
-      <div className="relative w-full h-60 overflow-hidden border bg-white rounded-md">
-        <iframe
-          title={`template-preview-${template.id}`}
-          srcDoc={template.content}
-          className="scale-[0.25] origin-top-left pointer-events-none w-[1400px] lg:w-[1600px] h-[1200px]"
-          sandbox=""
-        />
-      </div>
+      <div className="relative shadow-2xl">
+        <div className="relative w-full h-60 overflow-hidden border bg-white rounded-md">
+          <iframe
+            title={`template-preview-${template.id}`}
+            srcDoc={template.content}
+            className="scale-[0.25] origin-top-left pointer-events-none w-[1400px] lg:w-[1600px] h-[1200px]"
+            sandbox=""
+          />
+        </div>
 
-      <div className="bg-black/30  sm:opacity-0 hover:opacity-100 absolute inset-0 hover:bg-black/50 flex flex-col sm:flex-row items-center justify-center gap-2 rounded-md transition-colors">
+        <div className="bg-black/30  sm:opacity-0 hover:opacity-100 absolute inset-0 hover:bg-black/50 flex flex-col sm:flex-row items-center justify-center gap-2 rounded-md transition-colors">
           <button
             onClick={() => onEdit(template.id)}
-            className="px-4 py-2 text-sm font-medium text-gray-700 w-20 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors"
+            className="px-4 py-2 text-sm font-medium text-gray-700 shadow w-20 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors"
           >
             Edit
           </button>
           <button
             onClick={() => openModal(template.content)}
-            className="px-4 py-2 text-sm font-medium text-white bg-gray-800 w-20 rounded-md hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors"
+            className="px-4 py-2 text-sm font-medium text-white bg-gray-800 shadow w-20 rounded-md hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors"
           >
             Preview
           </button>
         </div>
-
-    </div>
+      </div>
       <div className="p-1">
         <h3 className="text-lg font-semibold text-gray-200 truncate">
           {template.name}
@@ -40,7 +38,6 @@ function TemplateCard({ template, onEdit, openModal }) {
         </p>
       </div>
     </div>
-    
   );
 }
 
