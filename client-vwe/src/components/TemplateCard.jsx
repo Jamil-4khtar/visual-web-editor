c69404ca576cbbdd19fc4ead45f61f5dc4b6febb
Jamil-4khtar <jamil4khtar@gmail.com>
2025-07-22 00:06:33 +0530

    fix: mobile size template card buttons opacity

--- client-vwe/src/components/TemplateCard.jsx
+++ client-vwe/src/components/TemplateCard.jsx
@@ -15,16 +15,16 @@ function TemplateCard({ template, onEdit, openModal }) {
         />
       </div>
 
-      <div className="opacity-0 hover:opacity-100 absolute inset-0 hover:bg-black/50 flex items-center justify-center space-x-3 rounded-md transition-colors">
+      <div className="bg-black/30  sm:opacity-0 hover:opacity-100 absolute inset-0 hover:bg-black/50 flex flex-col sm:flex-row items-center justify-center gap-2 rounded-md transition-colors">
           <button
             onClick={() => onEdit(template.id)}
-            className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors"
+            className="px-4 py-2 text-sm font-medium text-gray-700 w-20 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors"
           >
             Edit
           </button>
           <button
             onClick={() => openModal(template.content)}
-            className="px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded-md hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors"
+            className="px-4 py-2 text-sm font-medium text-white bg-gray-800 w-20 rounded-md hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors"
           >
             Preview
           </button>
